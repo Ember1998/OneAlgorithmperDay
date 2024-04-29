46acaf5bbd4fc2573746a9ab9085a1ee291cb7f1
Ember1998 <Arishamaharjan42@gmail.com>
2024-04-29 13:14:09 +0545

    SMS: Init Commit

---
+++ Algorithm&DataStruc/BinarySearch/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] arry = { 1, 2, 2, 3, 3, 4, 5, 56, 6, 6, 74, 5, 7, 8, 8, 534, 7, 78 };
+int i = 0; int j = arry.Length - 1;
+int midInx = arry.Length / 2;
+int.TryParse(Console.ReadLine(), out int num);
+
+while (num != arry[midInx])
+{
+    if (arry[midInx] > num)
+    {
+        j = midInx;
+    }
+    else if(arry[midInx] < num) 
+    {
+        i  = midInx;
+    }
+    else {
+        Console.WriteLine(midInx);
+    }
+    midInx = (i + j) / 2;
+}
+
---
+++ Algorithm&DataStruc/CustomArryClass/Array.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomArryClass
+{
+    public class Array
+    {
+        private int[] arr;
+        public Array()
+        {
+        }
+    }
+}
